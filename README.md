2142340d16869d69b4cc5374c2ea66d07a08745c
Donny Bereznak <dbereznak@gmail.com>
2025-05-28 15:48:00 -0400

    Initial upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Express Email
+
+This is just a small Express server. right now its used to handle submissions from my form
+
---
+++ email-server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { Resend } = require("resend");
+
+const app = express();
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+app.use(cors());
+app.get("/send", async (req, res) => {
+
+         const {data, error} = await resend.emails.send({
+            from: "info@dbereznak.dev",
+            to: "dbereznak@gmail.com",
+            subject: `New message from ${req.query.name}`,
+            html: `<h2>email: ${req.query.email}<h2><p>${req.query.message}</p>`
+        });
+
+        if (error) {
+            console.error("Error sending email:", error);
+            return res.status(400).json({ error });
+        }
+        
+        res.status(200).json({ data });
+});
+app.listen(3000, () => {
+    console.log("Email server is running on port 3000");
+});
